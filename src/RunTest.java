e015473d709c224759eac0fc4d525ce2e2d87f63
16saadc <16saadc@gmail.com>
2018-06-06 23:59:12 -0400

    complete

--- src/RunTest.java
+++ src/RunTest.java
@@ -1,5 +1,4 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,24 +8,27 @@ public class RunTest {
 
 
     public static void main(String[] args) {
-        // declaration and instantiation of objects/variables
-//        System.setProperty("webdriver.firefox.marionette","C:\\geckodriver.exe");
-//        WebDriver driver = new FirefoxDriver();
-        //comment the above 2 lines and uncomment below 2 lines to use Chrome
+
+        //set up chromedriver
         WebDriver driver = new ChromeDriver();
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/chromedriver");
 
-        String baseUrl = "https://www.mailinator.com";
+        // open mailinator
+        driver.get("https://www.mailinator.com");
 
-        driver.get(baseUrl);
-
+        //get a random email
         By b = By.xpath("/html/body/section[1]/div/div[3]/div[2]/div[2]/div[2]/h4[3]/span/a");
         String randomEmail = driver.findElement(b).getText();
         String username = randomEmail.substring(0, randomEmail.indexOf("@"));
         System.out.println(username);
 
-        driver.get("https://dashboard.hunchme.com/signup");
+        // need a random domain so that we can create new accounts on hunchme.com each time
+        driver.get("https://www.freevaluator.com/domain-name-generator/random-domain-generator");
+        By randomDomain = By.xpath("//*[@id='wrapper']/section[2]/div/div/div/div[2]/table/tbody/tr[1]/td[1]/a/strong");
+        String randDomain = driver.findElement(randomDomain).getText();
 
+        //fill in all hunchme.com sign up fields
+        driver.get("https://dashboard.hunchme.com/signup");
         By firstName = By.xpath("//*[@id='firstName_signup']");
         WebElement firstNameField = driver.findElement(firstName);
         firstNameField.sendKeys(username);
@@ -45,7 +47,7 @@ public class RunTest {
 
         By domain = By.xpath("//*[@id='domain_signup']");
         WebElement domainField = driver.findElement(domain);
-        domainField.sendKeys("http://mailinator.com");
+        domainField.sendKeys("http://" + randDomain + ".com");
 
         By dropdown = By.xpath("//*[@id='estimatedMonthlyPageviews_signup']");
         WebElement dropdownMenu = driver.findElement(dropdown);
@@ -56,26 +58,70 @@ public class RunTest {
         WebElement button = driver.findElement(signUpButton);
         button.click();
 
+        // wait a few seconds so that the activation email has time to be sent
+        try {
+            Thread.sleep(5000);
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+
         driver.get("https://www.mailinator.com/v2/inbox.jsp?zone=public&query=" + username);
-//        JavascriptExecutor js = (JavascriptExecutor) driver;
-//        js.executeScript("document.querySelector(\"//*[@id='inboxpane']/li/div\").click()");
+        try {
+            Thread.sleep(2000);
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+        By inbox = By.xpath("//*[@id='inboxpane']/li");
+        driver.findElement(inbox).click();
 
-        driver.findElement(By.cssSelector("li.all_message-item")).click();
-        //By inbox = By.xpath("//*[@id='inboxpane']/li/div");
+        //wait for message frame to show up
+        try {
+            Thread.sleep(2000);
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
 
-        //driver.findElement(inbox).click();
+        // switch driver to message frame in email
+        try {
+            driver.switchTo().frame("msg_body");
+        } catch (Exception e) {
+            System.out.println("Unable to open message body from inbox");
+        }
 
-//        List<WebElement> emails = driver.findElements(By.xpath("//*[@id='inboxpane']"));
-//        System.out.println(emails);
-//        for (WebElement e : emails){
-//            e.click();
-//        }
-        //Activation email sent
+        //click activation link
+        try {
+            By activateButton = By.xpath("/html/body/div/table/tbody/tr[2]/td/table/tbody/tr[2]/td/a");
+            driver.findElement(activateButton).click();
+        } catch (Exception e) {
+            System.out.println("Unable to click activation link on email");
+        }
 
-//*[@id="row_cluemcgee-1528308146-23758"]/div
+        try {
+            Thread.sleep(5000);
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+
+        try {
+            driver.findElement(By.xpath("//*[@id='setAdunitsBtn']")).click();
+        } catch (Exception e) {
+            System.out.println("Not able to click 'Create' button");
+        }
+        try {
+            Thread.sleep(5000);
+        } catch(InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+
+        try {
+            driver.findElement(By.xpath("//*[@id='logoutTab']/a")).click();
+        } catch (Exception e) {
+            System.out.println("unable to log out");
+        }
 
         //close Chrome
-        //driver.close();
+        System.out.println("task completed!");
+        driver.close();
 
     }
 
